33beeb62e852f8e83926452f8e555e6d4ed1fdbd
Nathaniel Davies <BorderDestroyerGame@gmail.com>
2025-12-27 18:04:31 -0500

    Bug Fix
    Fixed Error where the results from GetPosts.py would be dumped into the main folder

--- GetPosts.py
+++ GetPosts.py
@@ -13,17 +13,17 @@ def GetDatShit(subreddit_list):
         subreddit = reddit_shithole.subreddit(subreddit_name)
         
         posts = []
-        for post in subreddit.top(limit=20):
+        for post in subreddit.top(limit=50):
             posts.append([post.title, post.author, post.selftext])
         dataframe = pd.DataFrame(posts, columns=["Title", "Author", "Body"])
-        dataframe.to_csv(f"{subreddit_name}_top.csv", index=False)
+        dataframe.to_csv(f"./RedditLists/{subreddit_name}_top.csv", index=False)
         
         posts = []
-        for post in subreddit.hot(limit=20):
+        for post in subreddit.hot(limit=50):
             posts.append([post.title, post.author, post.selftext])
         dataframe = pd.DataFrame(posts, columns=["Title", "Author", "Body"])
-        dataframe.to_csv(f"{subreddit_name}_hot.csv", index=False)
+        dataframe.to_csv(f"./RedditLists/{subreddit_name}_hot.csv", index=False)
         
         print("One small step for reddit karma, one large step for AI kind")
 
-GetDatShit(["AmItheAsshole"])
+GetDatShit(["AmItheAsshole", "stories", "RedditStoryTime", "TrueScaryStories", "Ghoststories"])
